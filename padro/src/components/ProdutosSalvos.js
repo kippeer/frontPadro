c9af2f0961591f218e6e92b601c5e68c9da9a33e
Fabio <fabiot92@gmail.com>
2024-06-12 03:16:53 -0300

    formataçao download pdf

--- padro/src/components/ProdutosSalvos.js
+++ padro/src/components/ProdutosSalvos.js
@@ -1,22 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import jsPDF from 'jspdf';
 import html2canvas from 'html2canvas';
 import './ProdutosSalvos.css';
 
 function ProdutosSalvos({ produtos }) {
+  const [produtosList, setProdutosList] = useState(produtos);
+  const [selectedProducts, setSelectedProducts] = useState([]);
+  const [generatingPDF, setGeneratingPDF] = useState(false);
+
+  const handleSelect = (index) => {
+    if (selectedProducts.includes(index)) {
+      setSelectedProducts(selectedProducts.filter(i => i !== index));
+    } else {
+      setSelectedProducts([...selectedProducts, index]);
+    }
+  };
+
+  const handleDeleteSelected = () => {
+    const newProdutos = produtosList.filter((_, i) => !selectedProducts.includes(i));
+    setProdutosList(newProdutos);
+    setSelectedProducts([]);
+  };
 
   const downloadPDF = () => {
-    const input = document.getElementById('produtos-salvos-content');
-    html2canvas(input)
-      .then((canvas) => {
-        const imgData = canvas.toDataURL('image/png');
-        const pdf = new jsPDF('p', 'mm', 'a4');
-        const imgProps = pdf.getImageProperties(imgData);
-        const pdfWidth = pdf.internal.pageSize.getWidth();
-        const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
-        pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
-        pdf.save("produtos-salvos.pdf");
-      });
+    setGeneratingPDF(true);
+    setTimeout(() => {
+      const input = document.getElementById('produtos-salvos-content');
+      html2canvas(input)
+        .then((canvas) => {
+          const imgData = canvas.toDataURL('image/png');
+          const pdf = new jsPDF('p', 'mm', 'a4');
+          const imgProps = pdf.getImageProperties(imgData);
+          const pdfWidth = pdf.internal.pageSize.getWidth();
+          const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+          pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
+          pdf.save("produtos-salvos.pdf");
+          setGeneratingPDF(false);
+        });
+    }, 2000); // Atraso de 1 segundo antes de iniciar a geração do PDF
   };
 
   return (
@@ -24,8 +45,21 @@ function ProdutosSalvos({ produtos }) {
       <h2>Produtos Salvos</h2>
       <div id="produtos-salvos-content" className="pdf-content">
         <ul className="product-list">
-          {produtos.map((produto, index) => (
-            <li key={index} className="product-item">
+
+          <ul className='produtos-salvos'>Custo de Produção</ul>
+
+          {produtosList.map((produto, index) => (
+            <li
+              key={index}
+              className={`product-item ${selectedProducts.includes(index) ? 'selected' : ''} ${generatingPDF ? 'pdf-generating' : ''}`}
+            >
+              {!generatingPDF && (
+                <input
+                  type="checkbox"
+                  checked={selectedProducts.includes(index)}
+                  onChange={() => handleSelect(index)}
+                />
+              )}
               <strong>{produto.nome}</strong> - Custo Total: R${produto.custoTotal.toFixed(2)}
               <ul className="ingredient-list">
                 {produto.calculos.map((calculo, subIndex) => (
@@ -38,6 +72,7 @@ function ProdutosSalvos({ produtos }) {
           ))}
         </ul>
       </div>
+      <button onClick={handleDeleteSelected} className="delete-btn">Excluir Selecionados</button>
       <button onClick={downloadPDF} className="download-btn">Download PDF</button>
     </div>
   );
